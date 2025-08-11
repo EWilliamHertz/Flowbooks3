6f63fc16715627d260d787e4256596cba9ed2c3d
EWilliamHertz <74967501+EWilliamHertz@users.noreply.github.com>
2025-08-11 18:10:43 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js";
-import { getFunctions } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-functions.js"; // <-- NY IMPORT
+import { getFunctions } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-functions.js";
 
 // Din Firebase-konfiguration
 const firebaseConfig = {
@@ -23,4 +23,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const functions = getFunctions(app); // <-- NY EXPORT
+export const functions = getFunctions(app);
